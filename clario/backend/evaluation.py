fc8058749cdd41188ab2aa83f69982b618a7341e
NavinVenkatV <vnavinvenkat@gmail.com>
2026-07-06 10:53:07 +0200

    Cohere init

---
+++ clario/backend/evaluation.py
@@ -0,0 +1,3 @@
+#we done pdf to chunks to vector and we has users query has chunks and the chroma
+#comapres retreive the similar chunks to the query to exact so we are using Cohere that
+#solve the problem (not a problem - for a better solution)
